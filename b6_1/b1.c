f27eaadf4e2136d4074cda79541a1ab02726ae24
BigShark006 <nguyenvohoangphi.2006@gmail.com>
2025-12-24 10:01:16 +0700

    Update b1.c

--- b6_1/b1.c
+++ b6_1/b1.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 
-int main() 
+int main()
 {
-	int sockfd, len;
-	struct sockaddr_un address;
-	int result;
-	char ch='A';
+    int sockfd, len;
+    struct sockaddr_un address;
+    int result;
+    char ch = 'A';
 
-	sockfd =socket(AF_UNIX, SOCK_STREAM, 0);
-	address.sun_family=AF_UNIX;
-	strcpy(address.sun_path, "sever_socket");
-	len = sizeof(address);
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket failed");
+        exit(1);
+    }
 
-	result=connect(sockfd, (struct sockaddr*)&address, len);
-	if(result == -1)
-	{
-		perror("Oops: cilent1 problem");
-		exit(1);
-	}
+    memset(&address, 0, sizeof(address));
+    address.sun_family = AF_UNIX;
+    strcpy(address.sun_path, "server_socket");
+    len = sizeof(address);
 
-	write(sockfd, &ch, 1);
-	read(sockfd, &ch, 1);
-	printf("char from sever: %c\n", ch);
-	close(sockfd);
-	exit(0);
+    result = connect(sockfd, (struct sockaddr*)&address, len);
+    if (result == -1) {
+        perror("Oops: client1 problem");
+        exit(1);
+    }
+
+    write(sockfd, &ch, 1);
+    read(sockfd, &ch, 1);
+    printf("char from server: %c\n", ch);
+
+    close(sockfd);
+    return 0;
 }
