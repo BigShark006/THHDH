00a5d67637900c6eb6a59e89bb03e91b6b3ea0fd
BigShark006 <nguyenvohoangphi.2006@gmail.com>
2025-12-31 08:21:43 +0700

    Refactor process scheduling code for clarity

--- b9_2/b2.c
+++ b9_2/b2.c
@@ -1,94 +1,79 @@
-#include <bits/stdc++.h>
-#include <iostream>
-#include <climits>
-using namespace std;
+#include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
 
-struct Process
-{
-	int pid;
-       	int bt;
-       	int art;
+struct Process {
+    int pid;
+    int bt;
+    int art;
 };
 
-void findWT(Process proc[], int n, int wt[])
-{
-	int rt[n];
+void findWT(struct Process p[], int n, int wt[]) {
+    int rt[n];
+    for (int i = 0; i < n; i++)
+        rt[i] = p[i].bt;
 
-	for(int i = 0; i < n; i++)
-		rt[i] = proc[i].bt;
+    int complete = 0, time = 0;
+    int min = INT_MAX, shortest = 0;
+    bool found = false;
 
-	int complete = 0, t = 0, minm = INT_MAX;
-	int shortest = 0, finish_time;
-	bool check = false;
+    while (complete < n) {
+        for (int i = 0; i < n; i++) {
+            if (p[i].art <= time && rt[i] > 0 && rt[i] < min) {
+                min = rt[i];
+                shortest = i;
+                found = true;
+            }
+        }
 
-	while(complete != n)
-	{
-		for(int j = 0; j < n; j++)
-		{
-			if((proc[j].art <= t) && (rt[j] < minm) && rt[j] > 0)
-			{
-				minm = rt[j];
-				shortest = j;
-				check = true;
-			}
-		}
+        if (!found) {
+            time++;
+            continue;
+        }
 
-		if(check == false)
-		{
-			t++;
-			continue;
-		}
+        rt[shortest]--;
+        min = rt[shortest] == 0 ? INT_MAX : rt[shortest];
 
-		rt[shortest]--;
-		minm = rt[shortest];
-
-		if(minm == 0)
-			minm = INT_MAX;
-
-		if(rt[shortest] == 0)
-		{
-			complete++;
-			check = false;
-			finish_time = t + 1;
-			wt[shortest] = finish_time - proc[shortest].bt - proc[shortest].art;
-			if(wt[shortest] < 0)
-				wt[shortest] = 0;
-		}
-		
-		t++;
-	}
+        if (rt[shortest] == 0) {
+            complete++;
+            found = false;
+            int finish = time + 1;
+            wt[shortest] = finish - p[shortest].bt - p[shortest].art;
+            if (wt[shortest] < 0) wt[shortest] = 0;
+        }
+        time++;
+    }
 }
 
-void findTAT(Process proc[], int n, int wt[], int tat[])
-{
-	for(int i = 0; i < n; i++)
-		tat[i] = proc[i].bt + wt[i];
+void findTAT(struct Process p[], int n, int wt[], int tat[]) {
+    for (int i = 0; i < n; i++)
+        tat[i] = p[i].bt + wt[i];
 }
 
-void findAT(Process proc[], int n)
-{
-	int wt[n], tat[n], total_wt = 0, total_tat = 0;
-	findWT(proc, n, wt);
-	findTAT(proc, n, wt, tat);
+int main() {
+    struct Process p[] = {
+        {1, 6, 1},
+        {2, 8, 1},
+        {3, 7, 2},
+        {4, 3, 3}
+    };
 
-	cout << "Processes" << "Burst time" << "Waiting time" << "Turn around time\n" ;
+    int n = sizeof(p) / sizeof(p[0]);
+    int wt[n], tat[n];
+    int twt = 0, ttat = 0;
 
-	for(int i = 0; i < n; i++)
-	{
-		total_wt = total_wt + wt[i];
-		total_tat = total_tat + tat[i];
-		cout << " " << proc[i].pid << "\t\t" << proc[i].bt << "\t\t" << wt[i] << "\t\t" << tat[i] << endl;
-	}
+    findWT(p, n, wt);
+    findTAT(p, n, wt, tat);
 
-cout << "\nAverage waiting time = " << (float)total_wt / (float)n;
-cout << "\nAverage turn around time = " << (float)total_tat / (float)n;
-}
+    printf("PID\tBT\tAT\tWT\tTAT\n");
+    for (int i = 0; i < n; i++) {
+        twt += wt[i];
+        ttat += tat[i];
+        printf("%d\t%d\t%d\t%d\t%d\n",
+               p[i].pid, p[i].bt, p[i].art, wt[i], tat[i]);
+    }
 
-int main() 
-{
-	Process proc[] = {{1, 6, 1}, {2, 8, 1}, {3, 7, 2}, {4, 3, 3}};
-	int n = sizeof(proc) / sizeof(proc[0]);
-	findAT(proc, n);
-	
-	return 0;
+    printf("\nAverage Waiting Time = %.2f", (float)twt / n);
+    printf("\nAverage Turnaround Time = %.2f\n", (float)ttat / n);
+    return 0;
 }
